6a2be937ce8448b79a75b98e41c875eff8bee265
Ophiase <aaron.berriche@hotmail.fr>
2021-12-23 23:40:58 +0100

    Avancement map

--- game/state/Map.java
+++ game/state/Map.java
@@ -3,6 +3,11 @@ package game.state;
 /**
  * TODO : 
  * 
+ * Emplacement du voleur
+ * Emplacement des ports
+ * Emplacement des maisons
+ * Emplacement des routes
+ * Biomes
  * 
  */
 
@@ -10,15 +15,30 @@ public class Map {
 
     // --------------------
 
-    
+    private int size;
 
+    private int fiefIndex;
+    private int fiefX;
+    private int fiefY;
+
+    private int[][] biomes; // biomes
+    private int[][] houses; // maisons
+    private int[][] roadsH; // routes horizontals
+    private int[][] roadsV; // routes verticles
+    
     // --------------------
 
     public Map(game.Config conf) {
+        this.size = conf.getSizeOfMap();
+
         init();
     }
 
     private void init() {
 
     }
+
+    // --------------------
+
+    
 }
