c956a42a961e24755ed330f1b4b6d75a6a074acf
Ophiase <aaron.berriche@hotmail.fr>
2021-12-27 02:04:23 +0100

    working on opening of game

--- cli/CLI.java
+++ cli/CLI.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import game.Config;
 import game.Engine;
+import game.state.Player;
 import cli.actions.*;
 import cli.visuals.*;
 
@@ -100,16 +101,110 @@ public class CLI {
 
         // ---------------
 
+        // Pour chaque joueur placer première colonie/route
+        for (Player p: engine.getState().getPlayers())
+        {
+            System.out.println(p + " has to choose a road and a colony.");
+
+            if (p.isBot())
+                askBotFirstCity(p);
+            else
+                askUserFirstCity(p);
+
+            engine.endTurn();
+        }
+
+        // Pour chaque joueur placer seconde colonie/route
+        for (Player p: engine.getState().getPlayers())
+        {
+            System.out.println(p + " has to choose a road and a colony.");
+
+            if (p.isBot())
+                askBotFirstCity(p);
+            else
+                askUserFirstCity(p);
+
+            engine.endTurn();
+        }
+
+        System.out.println();
+        resume.showMap();
+        System.out.println();
+        Utils.delim();
+    }
+
+    private void askUserFirstCity(Player p) {
         System.out.println();
         resume.showMap();
         System.out.println();
         Utils.delim();
 
-        // ---------------
+        game.state.Map map = engine.getMap();
 
-        // Pour chaque joueur placer première colonie/route
-        for (int i = 0; i < config.getnParticipants(); i++) {
-            System.out.println("Do something ...");
+        System.out.println("Enter a location for your colony.");
+        System.out.println("> {x} {y}");
+        System.out.println("Exemple:");
+        System.out.println("> 1 3");
+
+        while (true)
+        {
+            try {
+                System.out.print("> ");
+                String[] args = sc.nextLine().split(" ");
+
+                if (args[0].startsWith("exit"))
+                    Utils.exit();
+
+                int who = p.getIndex();
+                int x = Integer.parseInt(args[0]);
+                int y = Integer.parseInt(args[1]);
+
+                if (!map.canBuyColony(who, x, y)) 
+                    throw new Exception();
+
+                map.canBuyColony(who, x, y);
+
+                break;
+            } catch (Exception e) {
+                System.out.println("Error, try again.");
+            }
         }
+
+        System.out.println("Enter a location for your road.");
+        System.out.println("> (h/v) {x} {y}");
+        System.out.println("Exemple:");
+        System.out.println("> h 1 3");
+
+        while (true)
+        {
+            try {
+                System.out.print("> ");
+                String[] args = sc.nextLine().split(" ");
+
+                if (args[0].startsWith("exit"))
+                    Utils.exit();
+
+                int who = p.getIndex();
+                boolean horizontal = args[0].toUpperCase().equals("H");
+                int x = Integer.parseInt(args[1]);
+                int y = Integer.parseInt(args[2]);
+
+                if (!map.canBuyRoad(who, horizontal, x, y)) 
+                    throw new Exception();
+
+                map.canBuyRoad(who, horizontal, x, y);
+
+                break;
+            } catch (Exception e) {
+                System.out.println("Error, try again.");
+            }
+        }
+
+    }
+
+    private void askBotFirstCity(Player p) {
+        
+        
+        System.out.println(p + " played ...");
     }
 }
